361061f0a8067089538d4660e20c83e0b585292c
umutdurn <umutdurn@gmail.com>
2017-12-27 23:07:28 +0200

    font-awesome add
    font-awesome add

--- filmProje/Global.asax.cs
+++ filmProje/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.UI;
 
 namespace filmProje
 {
@@ -29,6 +32,28 @@ namespace filmProje
                 writer.WriteLine("Hata Zamanı " + DateTime.Now.ToString() + "\n" + olusanHata.InnerException + "\n\n");
                 writer.Close();
             }
+        }
+
+        void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            HttpApplication app = sender as HttpApplication;
+            string acceptEncoding = app.Request.Headers["Accept-Encoding"];
+            Stream prevUncompressedStream = app.Response.Filter;
+
+            if (!(app.Context.CurrentHandler is Page ||
+                app.Context.CurrentHandler.GetType().Name == "SyncSessionlessHandler") ||
+                app.Request["HTTP_X_MICROSOFTAJAX"] != null)
+                return;
+
+            if (acceptEncoding == null || acceptEncoding.Length == 0)
+                return;
+
+            acceptEncoding = acceptEncoding.ToLower();
+
+            // gzip
+            app.Response.Filter = new GZipStream(prevUncompressedStream,
+                CompressionMode.Compress);
+            app.Response.AppendHeader("Content-Encoding", "gzip");
         }
 
 
